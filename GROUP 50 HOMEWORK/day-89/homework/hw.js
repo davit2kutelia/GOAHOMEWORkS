882c67daf0a32eaf64817a2c551ab1ef7ccf6129
davit2kutelia <datakutelia8@gmail.com>
2025-04-26 21:09:35 +0400

    second commit

---
+++ GROUP 50 HOMEWORK/day-89/homework/hw.js
@@ -0,0 +1,6 @@
+
+
+style="background-color: black;"><h1>full build</h1></ul>
+<ul class="" style="background-color: rgb(255, 0, 0);">CPU</ul>
+<ul class="" style="background-color: rgb(17, 0, 255);">GPU</ul>
+<ul class="" style="background-color: rgb(17, 255, 0);"
